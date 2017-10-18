59d79fa0d3e8c06846e5d3802157e2200ccb32fc
shbhuk <shbhuk@gmail.com>
2017-10-18 16:32:39 -0400

    Astropy Time object for utc_tdb. Time object errors
    Corrected time object scale error in utc_tdb.py and call to jpl ephemerides.

--- Scripts/BaryCorrPy.py
+++ Scripts/BaryCorrPy.py
@@ -3,6 +3,7 @@ from astropy.coordinates import EarthLocation
 from astropy.coordinates import solar_system_ephemeris
 from astropy.coordinates import get_body_barycentric_posvel
 from astropy.time import Time
+from astropy.coordinates import Angle
 import datetime
 from astropy.utils import iers
 import math
@@ -18,6 +19,8 @@ import sys
 
 location=os.getcwd()+'\\Box Sync\\BaryCorr\\'
 sys.path.append(location)
+from Eastman_applet import bvc,utc2bjd
+import PINT_erfautils as PINT
 import utc_tdb
 
 '''
@@ -27,6 +30,13 @@ JDUTC - Julian Date in UTC, eg 2450000.0
 '''
 #For Tau Ceti
 
+#t = Time("2014-09-22 23:22")
+#t=Time(datetime.datetime.now())
+
+
+
+JDUTC = Time(datetime.datetime.utcnow(),format='datetime',scale='utc')
+JDUTC=Time(2458000,format='jd',scale='utc')
 
 ra=1.734757638888889*15
 dec=-15.93955572
@@ -34,6 +44,9 @@ dec=-15.93955572
 lat=31.9599
 longi=-111.5997
 alt=2096
+
+loc=EarthLocation.from_geodetic(longi,lat,height=alt)
+
 
 R_ITRF =[1814985.3, -5213916.8, -3187738.1] # Needed only if Lat,Long and Alt are not specified. Values for CTIO # In meters
 
@@ -44,7 +57,8 @@ pmra = -1721.05 # Proper motion in RA in mas/year (scalar). Eg. PMRA = d(RA)/dt 
 pmdec = 854.16 # Proper motion in dec, in mas(year)
 
 px = 273.96 # Parallax in mas 
-rv = 00 # RV of Target in m/s
+rv = 0.0 # RV of Target in m/s
+zmeas=0.0
 
 
 
@@ -65,7 +79,7 @@ M_moon=73476730924573500000000 # Mass of the Moon in kg
 M_earth=u.M_earth.value
 M_sun=u.M_sun.value
 
-# ? , Mercury , Venus, Earth, Mars, Jupiter, Saturn , Uranus , Neptune, Moon, Sun
+#  Mercury , Venus, Earth, Mars, Jupiter, Saturn , Uranus , Neptune, Moon, Sun
 GM=[const.G*x for x in [0.3301e24,4.867e24,M_earth,0.6417e24,u.M_jup.value,568.5e24,86.82e24,102.4e24,M_moon,M_sun]]
 
 
@@ -78,12 +92,9 @@ Y=(u.M_earth.value/7.3476E22) # M_earth/M_moon
 '''
 
 
-t = Time("2014-09-22 23:22")
-t=Time(datetime.datetime.now())
 
-JDUTC = Time(datetime.datetime.utcnow()).jd
 
-JDTDB,TDB,JDTT=utc_tdb.JDUTC_to_JDTDB(JDUTC)
+JDTDB,JDTT=utc_tdb.JDUTC_to_JDTDB(JDUTC)
 ntimes=np.size(JDUTC)
 
 dpi=np.pi/180.
@@ -102,7 +113,7 @@ S=((1.0-F)**2)*CC
 A=6378137.0
 
 
-R_ITRF=[((A*CC+alt)*math.cos(Lat)*math.cos(Long)),((A*CC+alt)*math.cos(Lat)*math.sin(Long)),(A*S+alt)*math.sin(lat)] # meters
+#R_ITRF=[((A*CC+alt)*math.cos(Lat)*math.cos(Long)),((A*CC+alt)*math.cos(Lat)*math.sin(Long)),(A*S+alt)*math.sin(lat)] # meters
 
 EarthLocation.of_site('Kitt Peak')
 
@@ -110,19 +121,27 @@ EarthLocation.of_site('Kitt Peak')
 ######### NUTATION , PRECESSION , ETC. #############
 
 
-iers.IERS_A_README
+#iers.IERS_A_README
 
 
 r_eci=[10,10,10] # meters 
 v_eci=[0,0,0] # meters/second
 
+r_pint,v_pint=PINT.gcrs_posvel_from_itrf(loc,JDUTC)
+
+a=loc.get_gcrs_posvel(JDUTC)
+r_as=a[0].xyz.value
+v_as=a[1].xyz.value
+
+
+r_eci=r_pint[0]
+v_eci=v_pint[0]
+
+#r_eci=r_as
+#v_eci=v_as
 
 
 ######### EPHEMERIDES ######## 
-
-
-
-
 
 #eph=Ephemeris(de423)
 #print eph.names
@@ -130,26 +149,40 @@ v_eci=[0,0,0] # meters/second
 #solar_system_ephemeris.set('de423') 
 #print str(get_body_barycentric_posvel('earth',Time(TDB),ephemeris='de430'))  + '\t de'+str(430)+' ephemeris \n'  # Velocity in km / d
 #print str(get_body_barycentric_posvel('earth',Time(TDB),ephemeris='de432s'))  + '\t de'+str(432)+'s ephemeris \n'
-print str(get_body_barycentric_posvel('earth',Time(TDB),ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp')) + '\t de'+str(405)+ ' ephemeris \n'
 #print str(get_body_barycentric_posvel('earth',Time(TDB),ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de423_for_mercury_and_venus/de423.bsp')) + '\t de'+str(423)+ ' ephemeris \n'
 #print get_body_barycentric_posvel('earth',Time(TDB))  # Velocity in AU/d
-    
-earth_geo=get_body_barycentric_posvel('earth',Time(TDB),ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp')
+#print str(get_body_barycentric_posvel('earth',Time(TDB),ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp')) + '\t de'+str(405)+ ' ephemeris \n'    
+            
+earth_geo=get_body_barycentric_posvel('earth',JDTDB,ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp')
 r_obs=r_eci+earth_geo[0].xyz.value*1000. # meters
 v_geo=earth_geo[1].xyz.value*1000./(86400.)  # meters/second
 
 # Relativistic Addition of Velocities
 
-v_obs=(v_eci+v_geo)/(1.+v_eci*v_geo/c**2)
+v_obs=(v_eci+v_geo)/(1.+v_eci*v_geo/c**2) # m/s
 beta_earth=v_obs/c
 
+'''
+# From Jason
+r_obs  =  np.array( [ 142978285242.202545166016 ,  -44442754248.309997558594 ,  -19294519665.868061065674])
+earth_jason = [ 142978462.388793498278, -44448272.489472955465, -19291332.440149120986]
+vgeo_jason =  np.array(     [     9226.143475687091  ,        25732.150201600125       ,   11156.323841549474])
+v_eci =   np.array(     [    -402.387278631977  ,          -12.523024075409         ,     0.682872158243])
+v_obs=(v_eci+v_geo)/(1.+v_eci*v_geo/c**2) # m/s
+beta_earth=v_obs/c
 
+r_obs  =  np.array( [ 142978285242.202545166016 ,  -44442754248.309997558594 ,  -19294519665.868061065674])
+#earth_jason = [ 142978462.388793498278, -44448272.489472955465, -19291332.440149120986]
+vgeo_jason =  np.array(     [     9226.143475687091  ,        25732.150201600125       ,   11156.323841549474])
 
+v_eci =   np.array(     [    -402.387278631977  ,          -12.523024075409         ,     0.682872158243])
+v_obs=(v_eci+vgeo_jason)/(1.+v_eci*vgeo_jason/c**2) # m/s
+'''
 ######## Convert Star RA DEC to R0hat ####
 
 
 
-r0hat= np.tile(np.array([math.cos(ra*np.pi/180.)*math.cos(dec*np.pi/180.),math.sin(ra*np.pi/180.)*math.cos(dec*np.pi/180.),math.sin(dec*np.pi/180.)]),(ntimes,1))[0]
+r0hat=np.array([math.cos(ra*np.pi/180.)*math.cos(dec*np.pi/180.),math.sin(ra*np.pi/180.)*math.cos(dec*np.pi/180.),math.sin(dec*np.pi/180.)])
 
 up = [0.0,0.0,1.0] 
 east = np.cross(up,r0hat)
@@ -159,17 +192,17 @@ mu =  ((pmra*east+pmdec*north)/pctoau/1000) # Divided by 1000 since the Proper m
 
 #### Stellar position at each time
 
-epoch0 = 2000+(epoch-2451545.0)/365.25
-yearnow = 2000+(JDTDB+tbase-2451545.0)/365.25
+epoch0 = 2000.0+(epoch-2451545.0)/365.25
+yearnow = 2000.0+(JDTDB.jd+tbase-2451545.0)/365.25
 
 T = (yearnow-epoch0)                           #years
-vpi = rv/1.e3 * kmstoauyr * (px/1e3/pctoau)     # rad/yr
+vpi = rv/1.e3 * kmstoauyr * (px/1.e3/pctoau)     # rad/yr
 vel = mu + vpi*r0hat                           # rad/yr (m in AA)
 r = r0hat + vel*T                              # rad    (p1 in AA)
 rhat= r/math.sqrt(sum(r*r))
 
 
-px=1000    # DUMMY
+
 
 if px>0:
     rho=1000.0*rhat/px*pctoau-r_obs/AU # In AU
@@ -177,25 +210,50 @@ if px>0:
     r0=1000.0/px*pctoau*AU # In meters
     beta_star = r0*mu/c/year + rv*r0hat/c
     
-    zlighttravel = rv*r0*sum(mu*mu)*T/(c*c)
+    zlighttravel = rv*r0*sum(mu*mu)*T/(year*c*c)
 
 else:
     rhohat=rhat
     beta_star=0.0
     zlightravel = 0.0
+    
+# From Jason    
+jrobs = np.array([142978285242.202606201172  , -44442754248.309997558594 ,  -19294519665.868061065674]   )
+jr_eci =   np.array(  [    -177146.590880610427  ,      5518241.162959847599    ,   -3187225.718941929750])
+jvgeo =    np.array(  [      9226.143475687091    ,      25732.150201600125 ,         11156.323841549474])
+jv_eci = np.array(       [    -402.387278631987 ,           -12.523024075409      ,        0.682872152037])
+jx =       142978462.388793498278
+jy =       -44448272.489472955465
+jz =       -19291332.440149120986
+jjdtdb =         2458000.000800724141
+jjd_tt =         2458000.000800740905
+jjdutc =         2458000.000000000000
+jepoch =         2451545.000000000000
+jrho =       np.array(    [    650628.153802440153        ,      317510.145244374988     ,        -206710.960930745146])
+jrhohat =        np.array(         [   0.864162243459            ,       0.421715964536              ,    -0.274552840515])
+jr0 =   112632416328474496.000000000000
+jra =                   26.021364583333
+jdec =                  -15.939555722222
+jr0hat =      np.array(            [  0.864079927254 ,                  0.421838858305   ,               -0.274623118001])
+jrhat =      np.array(           [   0.864162668061     ,              0.421715157677           ,       -0.274552743412])
+jup =                   [ 0.000000000000            ,       0.000000000000           ,        1.000000000000]
+jeast =         np.array(      [    -0.438706260717          ,         0.898630522967           ,        0.000000000000])
+jnorth =  np.array(             [     0.246784716148          ,         0.120478881204          ,         0.961551945066])
+jmu =              np.array (    [ 0.000004682471              ,    -0.000006999158             ,      0.000003981868])
 
 
 ## Calculate Gravitaional Redshift due to SOlar system bodies
 
 bodies=solar_system_ephemeris.bodies
 
-calcndx=[bodies[1],bodies[0],bodies[7],bodies[8],bodies[2],bodies[4],bodies[9],bodies[10],bodies[6],bodies[3]]   # Sun, Earth, Jupiter, Saturn, Moon, Venus, Uranus, Neptune. #Excluding pluto
+#calcndx=[bodies[1],bodies[0],bodies[7],bodies[8],bodies[2],bodies[4],bodies[9],bodies[10],bodies[6],bodies[3]]   # Sun, Earth, Jupiter, Saturn, Moon, Venus, Uranus, Neptune. #Excluding pluto
 ss_bodies=[bodies[3],bodies[4],bodies[0],bodies[6],bodies[7],bodies[8],bodies[9],bodies[10],bodies[2],bodies[1]]   # Mercury , Venus, Earth, Mars, Jupiter, Saturn , Uranus , Neptune, Moon, Sun
 
 Sum_GR=0.0
 zshapiro=0.0
+
 for i in range(0,len(ss_bodies)-1):
-    jplephem=get_body_barycentric_posvel(ss_bodies[i],Time(TDB),ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp')
+    jplephem=get_body_barycentric_posvel(ss_bodies[i],JDTDB,ephemeris='https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp')
     pos_obj=jplephem[0].xyz.value*1000. # meters
     vel_obj=jplephem[1].xyz.value*1000./(86400.) # meters/second
     
@@ -206,18 +264,30 @@ for i in range(0,len(ss_bodies)-1):
     
     # Add Shapiro Delay
     a=np.dot((rhohat-np.dot(Xhat,rhohat)*Xhat),beta_earth)
-    zshapiro+= -2.0*GM[i]*(beta_earth*a)/((c*c)*(Xmag*(1+np.dot(Xhat,rhohat))))
+    zshapiro+= -2.0*GM[i]*a/((c*c)*(Xmag*(1+np.dot(Xhat,rhohat))))
     
     if Xmag!=0.0:
         Sum_GR+=GM[i]/Xmag  # (m/s)^2
     
 zgravity=1.0/(1+Sum_GR/(c*c))-1
 
+
+
 #determine the Barycentric RV correction (eq 28)
 
 gamma_earth=1./math.sqrt(1.-sum(beta_earth**2))
-zb = -1.0 - zshapiro - zlighttravel + gamma_earth*(1+np.dot(beta_earth,rhohat))*(1+np.dot(r0hat,beta_star))/((1+np.dot(beta_star,rhohat))*(1+zgravity))
 
+
+zb = -1.0 - zshapiro - zlighttravel + gamma_earth*(1+np.dot(beta_earth,rhohat))*(1+np.dot(r0hat,beta_star))/((1+np.dot(beta_star,rhohat))*(1+zgravity)) # Eq 28
+
+
+v_final=c*((1.0+zb)*(1.0+zmeas)-1.0)
+
+
+
+res = bvc(jd_utc=JDUTC.jd, ra=ra, dec=dec, lat=lat, lon=longi, elevation=alt,pmra=pmra,pmdec=pmdec,parallax=px,rv=rv,zmeas=0.0, epoch=epoch)
+
+print v_final,res
 
     
     
--- Scripts/utc_tdb.py
+++ Scripts/utc_tdb.py
@@ -10,9 +10,7 @@ import math
 
 def JDUTC_to_JDTDB(utctime):
     '''
-    Enter utc time as a scalar for JD or as an array in this format [year,month,day,hour,minute,second]
-    
-    Does not accept Astropy Time Object or datetime.datetime as input
+    Enter UTC time as Astropy Time Object
     
     
     Output:
@@ -30,7 +28,7 @@ def JDUTC_to_JDTDB(utctime):
     
     file_history=now-utc_tai  # How old is the file?
     
-    if file_history>datetime.timedelta(days=10):
+    if file_history>datetime.timedelta(days=20):
         print 'File more than 10 day old'
         openURL = urllib.URLopener()
         openURL.retrieve(url, location)
@@ -47,17 +45,15 @@ def JDUTC_to_JDTDB(utctime):
         offset.append(float(a[6]))
     f.close()
     
+    if type(utctime)!=astropy.time.core.Time: # Check if it is in Astropy Time Object
+        print "Input Time should be as Astropy Time Object"
+        raise TypeError("Input Time should be as Astropy Time Object")
     
-    if np.size(utctime)==1:    # Check whether date entered is in JD
-        JDUTC=Time(utctime,format='jd')
-        check_time=JDUTC.datetime
-        JDUTC=JDUTC.jd  # Convert to float instead of astropy time structure for comparison with np.where
-
     else:
-        check_time=(datetime.datetime(utctime[0],utctime[1],utctime[2],utctime[3],utctime[4],utctime[5]))
-        JDUTC=Time(check_time).jd
-        
-    print '\nConverting UTC time = '+str(check_time) +' \nJDUTC = '+ (str(JDUTC)) 
+        JDUTC=utctime.jd
+        check_time=utctime.datetime
+      
+    #print '\nConverting UTC time = '+str(check_time) +' \nJDUTC = '+ (str(JDUTC)) 
     
 
     tai_utc=offset[np.max(np.where(JDUTC>=np.array(jd)))]  # Leap second offset value to convert UTC to TAI
@@ -67,14 +63,16 @@ def JDUTC_to_JDTDB(utctime):
 
     g=(357.53+0.9856003*( JDUTC - 2451545.0 ))*np.pi/180.  # Earth's mean anomaly
     TDB = new_tt+datetime.timedelta(seconds=0.001658*np.sin(g)+0.000014*np.sin(2*g)) # TT to TDB
-    JDTT=Time(new_tt).jd
-    JDTDB = Time(TDB).jd
+    JDTT=Time(new_tt,scale='tt',format='datetime')
+    JDTT.format='jd'
+    JDTDB = Time(TDB,scale='tdb',format='datetime')
+    JDTDB.format='jd'
 
     #print '\nJDTDB = '+str(JDTDB)
     #print 'TDB = '+str(TDB)    
     #print 'JDTT = '+str(JDTT)
    
-    return JDTDB,TDB,JDTT
+    return JDTDB,JDTT
 
 
 
