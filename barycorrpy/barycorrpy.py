c7373ec20ae2fe538cbacb40120f1094f2329d12
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2018-01-11 00:37:56 +0100

    Squared brackets for units in comments

--- barycorrpy/barycorrpy.py
+++ barycorrpy/barycorrpy.py
@@ -34,20 +34,20 @@ def get_BC_vel(JDUTC, hip_id=0, ra=0.0, dec=0.0, epoch=2451545.0, pmra=0.0, pmde
         hip_id : Hipparcos Catalog ID. (Integer) . Epoch will be taken to be Catalogue Epoch or J1991.25
                 If specified then ra,dec,pmra,pmdec,px, and epoch need not be specified.
                                 OR
-        ra , dec : RA and Dec of star in degrees         
+        ra, dec : RA and Dec of star [degrees]
         epoch : Epoch of coordinates in Julian Date. Default is J2000 or 2451545.0
-        pmra : Proper motion in RA, in mas/year. Eg. PMRA = d(RA)/dt * cos(dec). Default is 0.0
-        pmdec : Proper motion in Dec, in mas/year. Default is 0.0
-        px : Parallax of target in MAS. Default is 0.0
+        pmra : Proper motion in RA [mas/year]. Eg. PMRA = d(RA)/dt * cos(dec). Default is 0.0
+        pmdec : Proper motion in Dec [mas/year]. Default is 0.0
+        px : Parallax of target [MAS]. Default is 0.0
                                 
         obsname : Name of Observatory as defined in Astropy EarthLocation routine. Can check list by EarthLocation.get_site_names(). 
                   If obsname is not used, then can enter lat,long,alt.
                                 OR 
-        lat : Latitude of observatory in degrees. North (+ve) and South (-ve)
-        longi : Longitude of observatory in degrees. East (+ve) and West (-ve)
-        alt : Altitude of observatory in meters.
+        lat : Latitude of observatory in [degrees]. North (+ve) and South (-ve)
+        longi : Longitude of observatory [degrees]. East (+ve) and West (-ve)
+        alt : Altitude of observatory [m].
         
-        rv : Radial Velocity of Target in m/s. Default is 0.0
+        rv : Radial Velocity of Target [m/s]. Default is 0.0
         zmeas : Measured redshift (e.g., the result of cross correlation with template spectrum). Default is 0.0
         ephemeris : Name of Ephemeris to be used. List of Ephemeris as queried by jplephem. Default is DE430. 
                     For first use Astropy will download the Ephemeris ( for DE430 ~100MB). Options for ephemeris inputs are 
@@ -123,19 +123,19 @@ def BCPy(JDUTC, ra=0.0, dec=0.0, lat=0.0, longi=0.0, alt=0.0, loc=0.0, epoch=245
     '''
     
     AU = const.astronomical_unit # 1 AU in meters
-    c = const.c # Speed of light in m/s
+    c = const.c # Speed of light [m/s]
     pctoau = 3600*180/np.pi # No. of AU in one parsec
     year = 365.25*3600*24 # 1 year in seconds
     kmstoauyr = year/(1000*AU)
     
-    M_moon = 73476730924573500000000 # Mass of the Moon in kg
+    M_moon = 73476730924573500000000 # Mass of the Moon [kg]
     M_earth = u.M_earth.value
     M_sun = u.M_sun.value
     
     # Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn , Uranus , Neptune, Moon
     GM = [const.G*x for x in [M_sun, 0.3301e24, 4.867e24, M_earth, 0.6417e24, u.M_jup.value, 568.5e24, 86.82e24, 102.4e24, M_moon]]
     
-    # Convert times to obtain TDB and TT 
+    # Convert times to obtain TDB and TT
     JDTDB, JDTT, warning, error = utc_tdb.JDUTC_to_JDTDB(JDUTC, fpath=leap_dir, leap_update=leap_update)
     
     
@@ -143,14 +143,14 @@ def BCPy(JDUTC, ra=0.0, dec=0.0, lat=0.0, longi=0.0, alt=0.0, loc=0.0, epoch=245
     
     r_pint, v_pint = PINT.gcrs_posvel_from_itrf(loc, JDUTC, JDTT)
     
-    r_eci = r_pint[0]  # meters
-    v_eci = v_pint[0]  # meters/second
+    r_eci = r_pint[0]  # [m]
+    v_eci = v_pint[0]  # [m/s]
     
     ##### EPHEMERIDES #####
             
-    earth_geo = get_body_barycentric_posvel('earth', JDTDB, ephemeris=ephemeris) # km
-    r_obs = r_eci + earth_geo[0].xyz.value*1000. # meters
-    v_geo = earth_geo[1].xyz.value*1000./86400.  # m/s
+    earth_geo = get_body_barycentric_posvel('earth', JDTDB, ephemeris=ephemeris) # [km]
+    r_obs = r_eci + earth_geo[0].xyz.value*1000. # [m]
+    v_geo = earth_geo[1].xyz.value*1000./86400.  # [m/s]
     
     # Relativistic Addition of Velocities
     v_obs = (v_eci+v_geo) / (1.+v_eci*v_geo/c**2) # m/s
@@ -173,17 +173,17 @@ def BCPy(JDUTC, ra=0.0, dec=0.0, lat=0.0, longi=0.0, alt=0.0, loc=0.0, epoch=245
     epoch0 = 2000.0 + (epoch-2451545.0)/365.25
     yearnow = 2000.0 + (JDTDB.jd-2451545.0)/365.25
     
-    T = yearnow - epoch0                           # years
-    vpi = rv/1.e3 * kmstoauyr * (px/1.e3/pctoau)   # rad/yr
-    vel = mu + vpi*r0hat                           # rad/yr (m in AA)
-    r = r0hat + vel*T                              # rad    (p1 in AA)
+    T = yearnow - epoch0                           # [years]
+    vpi = rv/1.e3 * kmstoauyr * (px/1.e3/pctoau)   # [rad/yr]
+    vel = mu + vpi*r0hat                           # [rad/yr] (m in AA)
+    r = r0hat + vel*T                              # [rad]    (p1 in AA)
     rhat = r / math.sqrt(sum(r*r))
     
     # Parallax correction 
     if px>0:
-        rho = 1000.0*rhat/px*pctoau-r_obs/AU # In AU
+        rho = 1000.0*rhat/px*pctoau - r_obs/AU # [AU]
         rhohat = rho / math.sqrt(sum(rho*rho)) # Unitless
-        r0 = 1000.0/px*pctoau*AU # In meters
+        r0 = 1000.0/px*pctoau*AU # [m]
         beta_star = r0*mu/c/year + rv*r0hat/c
         
         zlighttravel = rv*r0*sum(mu*mu)*T/(year*c*c)
@@ -197,18 +197,18 @@ def BCPy(JDUTC, ra=0.0, dec=0.0, lat=0.0, longi=0.0, alt=0.0, loc=0.0, epoch=245
     ##### Calculate Gravitaional Redshift due to Solar system bodies #####
     
     bodies = solar_system_ephemeris.bodies
-    ss_bodies = [bodies[i] for i in [1, 3, 4, 0, 6, 7, 8, 9, 10, 2]]   # Sun, Mercury , Venus, Earth, Mars, Jupiter, Saturn , Uranus , Neptune, Moon
+    ss_bodies = [bodies[i] for i in [1, 3, 4, 0, 6, 7, 8, 9, 10, 2]]   # Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Moon
     
     Sum_GR = 0.
     zshapiro = 0.
     
     for i in range(len(ss_bodies)):
         jplephem = get_body_barycentric(ss_bodies[i], JDTDB, ephemeris=ephemeris)
-        pos_obj = jplephem.xyz.value*1000. # meters
+        pos_obj = jplephem.xyz.value*1000. # [m]
         
         # Vector from object barycenter to Observatory
         X = np.array(r_obs-pos_obj)
-        Xmag = math.sqrt(sum(X*X)) # In meters
+        Xmag = math.sqrt(sum(X*X)) # [m]
         Xhat = X / Xmag # Unitless
         
         # Add Shapiro Delay
@@ -216,7 +216,7 @@ def BCPy(JDUTC, ra=0.0, dec=0.0, lat=0.0, longi=0.0, alt=0.0, loc=0.0, epoch=245
         zshapiro += -2.*GM[i]*a / ((c*c)*(Xmag*(1+np.dot(Xhat, rhohat))))   # Eq 27
         
         if Xmag!=0.:
-            Sum_GR += GM[i]/Xmag  # (m/s)^2
+            Sum_GR += GM[i]/Xmag  # [(m/s)^2]
         
     zgravity = 1./(1+Sum_GR/(c*c)) - 1
     
@@ -226,7 +226,7 @@ def BCPy(JDUTC, ra=0.0, dec=0.0, lat=0.0, longi=0.0, alt=0.0, loc=0.0, epoch=245
     gamma_earth = 1. / math.sqrt(1.-sum(beta_earth**2))
     
     zb = -1.0 - zshapiro - zlighttravel + gamma_earth*(1+np.dot(beta_earth, rhohat))*(1+np.dot(r0hat, beta_star))/((1+np.dot(beta_star, rhohat))*(1+zgravity)) # Eq 28
-    v_final = c*((1.0+zb)*(1.0+zmeas)-1.0)  # m/s
+    v_final = c * ((1.0+zb)*(1.0+zmeas)-1.0)  # [m/s]
     
     
     return v_final, warning, error
