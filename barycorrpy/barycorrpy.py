a93f3f979ee47c18183f8ab926957dddd72e7476
shbhuk <shbhuk@gmail.com>
2018-01-14 18:35:17 -0500

    extra parameters - lat, long

--- barycorrpy/barycorrpy.py
+++ barycorrpy/barycorrpy.py
@@ -98,7 +98,7 @@ def get_BC_vel(JDUTC,hip_id=0,ra=0.0,dec=0.0,epoch=2451545.0,pmra=0.0,
 
     
     for i in range(0,np.size(JDUTC)):               
-        a=(BCPy(JDUTC=JDUTC[i],ra=ra,dec=dec,lat=lat,longi=longi,alt=alt,loc=loc,pmra=pmra,pmdec=pmdec,px=px,rv=rv,zmeas=zmeas,epoch=epoch,ephemeris=ephemeris,leap_dir=leap_dir,leap_update=leap_update))
+        a=(BCPy(JDUTC=JDUTC[i],ra=ra,dec=dec,loc=loc,pmra=pmra,pmdec=pmdec,px=px,rv=rv,zmeas=zmeas,epoch=epoch,ephemeris=ephemeris,leap_dir=leap_dir,leap_update=leap_update))
         vel.append(a[0])
         warning.append(a[1])
         error.append(a[2])
@@ -115,7 +115,7 @@ def get_BC_vel(JDUTC,hip_id=0,ra=0.0,dec=0.0,epoch=2451545.0,pmra=0.0,
 
 
 
-def BCPy(JDUTC,ra=0.0,dec=0.0,lat=0.0,longi=0.0,alt=0.0,loc=0.0,epoch=2451545.0,pmra=0.0,
+def BCPy(JDUTC,ra=0.0,dec=0.0,loc=0.0,epoch=2451545.0,pmra=0.0,
     pmdec=0.0,px=0.0,rv=0.0,zmeas=0.0,ephemeris='de430',leap_dir=os.path.dirname(__file__),leap_update = True ) :
     '''
     Barycentric Velocity Correction at the 1 cm/s level, as explained in Wright & Eastman, 2014.
