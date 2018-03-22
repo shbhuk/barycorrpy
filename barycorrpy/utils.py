0d3b1baab0c80fc37bc97a5b772c02d23f423e56
shbhuk <shbhuk@gmail.com>
2018-03-22 19:23:34 -0400

    stellar_query

--- barycorrpy/utils.py
+++ barycorrpy/utils.py
@@ -16,21 +16,31 @@ def flux_weighting(flux,qty):
     
     return sum(qty * flux)/sum(flux)
     
-def get_stellar_data(name='',use_hip_if_exists = True):
+def get_stellar_data(name=''):
     '''
     Function to query Simbad for following stellar information RA, Dec, PMRA, PMDec, Parallax Epoch
     INPUTS:
         name = Name of source. Example 
-        use_hip_if_exists = If source exists in Hipparcos catalog and has a HIP ID, use Hipparcos
     
     
     '''
+    warning = []
+    
     customSimbad = Simbad()
     customSimbad.add_votable_fields('ra(2;A;ICRS;J2000)', 'dec(2;D;ICRS;J2000)','pm', 'plx','parallax','rv_value')
     #Simbad.list_votable_fields()
     customSimbad.remove_votable_fields( 'coordinates')
     #Simbad.get_field_description('orv')
     obj = customSimbad.query_object(name)
+
+    # Check for masked values in the most convoluted way possible    
+    if all([not x for x in [obj.mask[0][i] for i in obj.colnames]])==False:
+        warning = 'Masked values present in queried dataset'
+        print(warning)
+
+    
+    obj = obj.filled(0)
+    
     
     pos = SkyCoord(ra=obj['RA_2_A_ICRS_J2000'],dec=obj['DEC_2_D_ICRS_J2000'],unit=(u.hourangle, u.deg))
     ra = pos.ra.value
@@ -40,6 +50,19 @@ def get_stellar_data(name='',use_hip_if_exists = True):
     plx = obj['PLX_VALUE'][0]
     rv = obj['RV_VALUE'][0]
     epoch = 2451545.0
+    
+    star = {}
+    star['ra'] = ra
+    star['dec'] = dec
+    star['pmra'] = pmra
+    star['pmdec'] = pmdec
+    star['plx'] = plx
+    star['rv'] = rv
+    star['epoch'] = epoch
+    
+    warning = 'Querying object {} from Simbad. Values used are RA:{} Dec:{} PMRA:{} PMDEC:{} Plx:{} RV:{} Epoch:{} '.format(name,ra,dec,pmra,pmdec,plx,rv,epoch)
+    print(warning)
+    
     
     return ra,dec,pmra,pmdec,plx,rv,epoch
   
