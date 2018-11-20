430fb4bb2406aeb0b519ee147474632e93041af3
Shubham <shbhuk@gmail.com>
2018-11-20 13:34:56 -0500

    New version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='barycorrpy',
-      version='0.2.1.1',
+      version='0.2.2',
       description='Barycentric Velocity correction at 1 cm/s level',
       long_description=readme(),
       url='https://github.com/shbhuk/barycorrpy',
