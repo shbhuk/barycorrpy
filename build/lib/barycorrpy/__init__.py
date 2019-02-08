96660da42dfcbef495b54a6526ad0937a2d3a6ff
shbhuk@gmail.com <shbhuk@gmail.com>
2019-02-08 11:00:37 -0500

    Generate v0.2.3 dist

--- build/lib/barycorrpy/__init__.py
+++ build/lib/barycorrpy/__init__.py
@@ -1,8 +1,9 @@
 from .utc_tdb import *
 from .PINT_erfautils import *
 from .utils import *
+from .tests import *
 from .sample_script import *
 from .barycorrpy import get_BC_vel,BCPy,exposure_meter_BC_vel
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.3'
