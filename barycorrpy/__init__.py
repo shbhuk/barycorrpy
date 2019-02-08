0ec1a289e856a3f213f7bbf4201911518dfba601
shbhuk@gmail.com <shbhuk@gmail.com>
2019-02-08 10:29:13 -0500

    Rename unit test directory

--- barycorrpy/__init__.py
+++ barycorrpy/__init__.py
@@ -1,6 +1,7 @@
 from .utc_tdb import *
 from .PINT_erfautils import *
 from .utils import *
+from .tests import *
 from .sample_script import *
 from .barycorrpy import get_BC_vel,BCPy,exposure_meter_BC_vel
 
